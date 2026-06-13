faa468db3a4d37bf8d23ab58af3bd6eb2a789fff
Jitendra Das <jitud9902@gmail.com>
2026-06-13 21:52:50 +0530

    Addt3

--- project/data.py
+++ project/data.py
@@ -22,7 +22,7 @@ def connect():
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["https://login-form-theta-rust.vercel.app/"],
+    allow_origins = ["https://login-form-theta-rust.vercel.app"],
     allow_credentials=True,
     allow_methods = ["*"],
     allow_headers = ["*"]
