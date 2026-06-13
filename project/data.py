a324b4655ce831041cd7d537891da0782b73d165
Jitendra Das <jitud9902@gmail.com>
2026-06-13 21:47:16 +0530

    Addt1

--- project/data.py
+++ project/data.py
@@ -23,6 +23,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins = ["login-form-theta-rust.vercel.app"],
+    allow_credentials=True,
     allow_methods = ["*"],
     allow_headers = ["*"]
 )
