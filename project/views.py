9fa38671068e3ff20c3040664fb110370d8827b2
Jitendra Das <jitud9902@gmail.com>
2026-06-13 23:07:32 +0530

    Addt7

--- project/views.py
+++ project/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+import json
+
+def register(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        userName = data.get("userName")
+        password = data.get("password")
+
+        # yahan database insert karo
+
+        return JsonResponse({
+            "msg": "success",
+            "userName": userName
+        })
+
+    return JsonResponse({"error": "Invalid request"})
